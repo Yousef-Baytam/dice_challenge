727d3f9468622dd6d85d0192d01a1bd1dd727091
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-23 10:25:50 +0300

    Added a function to generate a random img

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 const imgs = document.querySelectorAll('img')
+const randomSrc = ['./images/dice1.png', './images/dice2.png', './images/dice3.png', './images/dice4.png', './images/dice5.png', './images/dice6.png']
+
+const randomImg = () => randomSrc[Math.floor(Math.random() * randomImg.length)]
+
+
