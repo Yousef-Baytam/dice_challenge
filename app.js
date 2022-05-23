934b6945f850dc297dd9710bdb0ba1e225ea3ccf
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-23 10:57:33 +0300

    Moved the game to a function and added a button

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const imgs = document.querySelectorAll('img')
 const randomSrc = ['./images/dice1.png', './images/dice2.png', './images/dice3.png', './images/dice4.png', './images/dice5.png', './images/dice6.png']
 const h1 = document.querySelector('h1')
+const button = document.createElement('button')
 
 const randomImg = () => randomSrc[Math.floor(Math.random() * randomSrc.length)]
 
-for (let img of imgs) {
-    img.src = `${ randomImg() }`
-}
-let img1 = parseInt(imgs[0].src.slice(33, 34))
-let img2 = parseInt(imgs[1].src.slice(33, 34))
+const shuffle = () => {
+    for (let img of imgs) {
+        img.src = `${ randomImg() }`
+    }
+    let img1 = parseInt(imgs[0].src.slice(33, 34))
+    let img2 = parseInt(imgs[1].src.slice(33, 34))
 
-img1 > img2 ? h1.innerText = 'Player 1 Wins'
-    : img1 < img2 ? h1.innerText = 'Player 2 Wins'
-        : h1.innerText = 'Draw'
+    img1 > img2 ? h1.innerText = 'Player 1 Wins!'
+        : img1 < img2 ? h1.innerText = 'Player 2 Wins!'
+            : h1.innerText = 'Draw!'
+}
+
+document.body.appendChild(button)
+
+shuffle()
