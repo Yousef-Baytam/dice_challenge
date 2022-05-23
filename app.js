5be790c619d2085a389c8996da28c18b80af21b7
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-23 10:43:17 +0300

    Added vars for the img srcs

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const imgs = document.querySelectorAll('img')
 const randomSrc = ['./images/dice1.png', './images/dice2.png', './images/dice3.png', './images/dice4.png', './images/dice5.png', './images/dice6.png']
-const h1 = document.querySelectorAll('h1')
+const h1 = document.querySelector('h1')
 
 const randomImg = () => randomSrc[Math.floor(Math.random() * randomSrc.length)]
 
 for (let img of imgs) {
     img.src = `${ randomImg() }`
 }
+let Img1 = parseInt(imgs[0].src.slice(33, 34))
+let Img2 = parseInt(imgs[1].src.slice(33, 34))
