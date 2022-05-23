b1745b5f2c21bc7a63b1dba55f24c4ba84a223a0
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-05-23 10:30:10 +0300

    Mapped the random function to the imgs src

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const imgs = document.querySelectorAll('img')
 const randomSrc = ['./images/dice1.png', './images/dice2.png', './images/dice3.png', './images/dice4.png', './images/dice5.png', './images/dice6.png']
+const h1 = document.querySelectorAll('h1')
 
-const randomImg = () => randomSrc[Math.floor(Math.random() * randomImg.length)]
+const randomImg = () => randomSrc[Math.floor(Math.random() * randomSrc.length)]
 
-
+for (let img of imgs) {
+    img.src = `${ randomImg() }`
+}
